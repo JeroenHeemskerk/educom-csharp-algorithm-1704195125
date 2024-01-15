9b8c1acaa8953685ece35da9d4c75377ed8e3617
Nicole Goris <nicolegoris@hotmail.nl>
2024-01-15 18:04:01 +0100

    #11 mvc toegevoegd

---
+++ BornToMove.ASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BornToMove.ASPNET.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
